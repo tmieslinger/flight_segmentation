a7eda52c8b7dc532841987a38afd085eba540abc
Theresa Mieslinger <theresa.mieslinger@gmail.com>
2024-09-12 15:08:30 -0400

    use root ipfs CID to get BAHAMAS data

--- scripts/navdata.py
+++ scripts/navdata.py
@@ -12,18 +12,9 @@ def get_navdata_HALO(flight):
     from intake import open_catalog
     import orcestra.postprocess.level0
 
-    bahamas_cids = {
-        "HALO-20240809b": "QmahYozz3StbbeJxXn7zPycdZYz6mLVNYszEU28XqxSMGc",
-        "HALO-20240811a": "QmbmtXr3pSGexuteUAcasgAzSHHpfKNXk9r5JfZXKqa2d5",
-        "HALO-20240813a": "QmcFHpX6zNcG7kFjUNff8BEUkoYomwpnTJUAjhXq9KACtg",
-        "HALO-20240816a": "QmTCph5sHoq9pcXLHyHix2qAVmSCBjvLmbPCfx2QgVs13a",
-        "HALO-20240818a": "QmSjsEFDywceEDLxs2zHfcj1GuRATDosgw9fwsFT5bAX8x",
-        "HALO-20240821a": "QmXnuuipS3xFE3mX7ZGRti55NapwSRVsBMPDfvnTMkSLoj",
-        "HALO-20240822a": "QmethFGpJ5jg8ASnS3kcQPDN6bct4g85DBh2HWQQqj7DXb",
-    }
+    root = "ipfs://QmcqgdpTAXRJFnKonSUWDDbpJfiLbq3275GHn84sizNEfm"
+    ds = xr.open_dataset(f"{root}/products/HALO/bahamas/ql/{flight}.zarr", engine="zarr")#.pipe(orcestra.postprocess.level0.bahamas)
 
-    ds = xr.open_dataset(f"ipfs://{bahamas_cids[flight]}", engine="zarr").pipe(orcestra.postprocess.level0.bahamas)
-    print(ds)
     return xr.Dataset({
         "time": ds.time,
         "lat": ds.IRS_LAT,
